f3f2d5c579a2dcf3636424fb1663959a653d28e1
Léa Fizzala <leafizzala.dev@gmail.com>
2022-11-29 14:49:04 +0100

    Created repo for CadavreExquis entity

---
+++ src/Repository/CadavreExquisRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\CadavreExquis;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\QueryBuilder;
+
+/**
+ * @extends ServiceEntityRepository<CadavreExquis>
+ *
+ * @method CadavreExquis|null find($id, $lockMode = null, $lockVersion = null)
+ * @method CadavreExquis|null findOneBy(array $criteria, array $orderBy = null)
+ * @method CadavreExquis[]    findAll()
+ * @method CadavreExquis[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CadavreExquisRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Annonce::class);
+    }
+
+    public function save(Annonce $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Annonce $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+}
